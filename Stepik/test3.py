1c0aeff830c62fc5352ad9dd2be354b1f5558ab9
Terekhinia <mannergam91@mail.ru>
2022-09-30 01:19:40 +0300

    init

---
+++ Stepik/test3.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+def test3():
+    link = "http://suninjuly.github.io/registration1.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+    browser.implicitly_wait(10)
+    input1 = browser.find_element(By.NAME, 'first_name*')
+    input1.send_keys("Ivan")
+    input2 = browser.find_element(By.NAME, 'last_name*')
+    input2.send_keys("Petrov")
+    input3 = browser.find_element(By.NAME, 'firstname*')
+    input3.send_keys("Smolensk")
+    button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+    button.click()
+    # time.sleep(1)
+    welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
+    welcome_text = welcome_text_elt.text
+    assert "Congratulations! You have successfully registered!" == welcome_text
+    time.sleep(10)
+    browser.quit()
+
+
+test3()
