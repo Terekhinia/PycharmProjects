fc71a1ae091bd88462bce0b1b8577b08ae8c9709
Terekhinia <mannergam91@mail.ru>
2022-10-16 01:02:33 +0300

    init

--- Stepik/Drift.py
+++ Stepik/Drift.py
@@ -1,12 +1,5 @@
+import time
+import math
 
-
-# class Student:
-#     name = 'unknown' # class attribute
-#     def __init__(self):
-#         self.age = 20  # instance attribute
-#
-#     @classmethod
-#     def tostring(cls):
-#         print('Student Class Attributes: name=',cls.name)
-
-
+answer = math.log(int(time.time()))
+print(answer)
