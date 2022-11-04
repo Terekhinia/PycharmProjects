deaa0b351e7ddab1eb6943147080c545d74cb010
Terekhinia <mannergam91@mail.ru>
2022-11-04 03:00:30 +0300

    init

--- page_object/pages/base_page.py
+++ page_object/pages/base_page.py
@@ -3,14 +3,15 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 from selenium.common.exceptions import TimeoutException
+from .locators import BasePageLocators
 import math
 
 class BasePage():
 
-    def __init__(self, browser, url="http://selenium1py.pythonanywhere.com", timeout=10):
+    def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
-        # self.browser.implicitly_wait(timeout)
+        self.browser.implicitly_wait(timeout)
 
     def open(self):
         self.browser.get(self.url)
@@ -36,6 +37,13 @@ class BasePage():
         except TimeoutException:
             return False
         return True
+
+    # def go_to_login_page(self):
+    #     link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+    #     link.click()
+    #
+    # def should_be_login_link(self):
+    #     assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Линк отсутствует"
 
     # def solve_quiz_and_get_code(self):
     #     alert = self.browser.switch_to.alert
